b404a825840fe1d221cd4913afe601e4a2c97df4
AdeelZahid <m.adeel2144@gmail.com>
2021-02-27 00:51:00 +0500

    Drawer Items has been successfully implemented ..

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  ProfileScreen({Key key}) : super(key: key);
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        controller: null,
+        child: Column(
+          children: [
+            Stack(),
+          ],
+        ),
+      ),
+    );
+  }
+}
