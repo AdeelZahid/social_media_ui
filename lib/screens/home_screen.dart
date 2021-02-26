24fdb9284addc989f246fbcba9a3ea092e9d5b48
AdeelZahid <m.adeel2144@gmail.com>
2021-02-26 21:49:01 +0500

    post carousel screen almost completed ..

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_ui/data/data.dart';
 import 'package:social_media_ui/widgets/following_user.dart';
+import 'package:social_media_ui/widgets/post_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -11,11 +13,13 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
+  PageController _pageController;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
+    _pageController = PageController(initialPage: 0, viewportFraction: 0.8);
     // _tabController.index;
   }
 
@@ -59,6 +63,11 @@ class _HomeScreenState extends State<HomeScreen>
       body: ListView(
         children: [
           FollowingUsers(),
+          PostsCarousel(
+            pageController: _pageController,
+            title: 'Post',
+            posts: posts,
+          ),
         ],
       ),
     );
